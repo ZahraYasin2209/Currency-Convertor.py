f4ab552977085af5f4204ed5fe68462d3938068d
Zahra Yasin <109916495+ZahraYasin2209@users.noreply.github.com>
2023-09-14 09:47:48 -0700

    Add files via upload

---
+++ Currency Convertor.py
@@ -0,0 +1,19 @@
+# Currency Convertor
+# Pakistani currency to other currencies
+
+with open('CurrencyNotes.txt') as f:
+	lines = f.readlines()
+
+currencyDict = {}
+
+for line in lines:
+	parsed = line.split("\t")
+	currencyDict[parsed[0]] = parsed[1]
+
+amount = int(input("Enter amount:\n"))
+
+print("Enter the name of the currency you want to convert this amount to? Available Options:\n")
+
+[print(item) for item in currencyDict.keys()]
+currency = input("Please enter one of these values: \n")
+print(f"{amount} PKR is equal to {amount *float(currencyDict[currency])} {currency}")
